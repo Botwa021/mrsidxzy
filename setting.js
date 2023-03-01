3b9f311d7283d40561625b72d8ea4ae40d40f635
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2023-03-01 18:08:02 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const chalk = require('chalk')
-global.owner = ['6281395861695']
-global.ownernomer = "6281395861695"
-global.premium = ['6281395861695']
-global.packname = 'Sticker By'
-global.author = 'BOTCAHX'
+global.owner = ['6281248009076']
+global.ownernomer = "6281248009076"
+global.premium = ['6288233832771']
+global.packname = '2023'
+global.author = 'BALLBOT'
 global.sessionName = 'session'
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -12,11 +12,11 @@ global.jumlah = '1000000000'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
 global.mess = {
-    success: '✅Done',
+    success: 'Done',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
     premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
-    owner: 'Fitur Khusus Owner Bot',
+    owner: 'Fitur Khusus Owner Dela',
     group: 'Fitur Digunakan Hanya Untuk Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
@@ -32,13 +32,13 @@ global.limitawal = {
 }
 global.thumb = fs.readFileSync('./media/image/tio.jpg')
 global.faall = fs.readFileSync('./media/image/fake.jpg')
-global.mygit = 'https://github.com/BOTCAHX'
-global.webmy = 'https://api.botcahx.biz.id'
-global.ghme = 'https://github.com/BOTCAHX'
-global.gcbot = "https://api.botcahx.biz.id"
-global.botname = 'BOTCAHX'
-global.omlen = '© TioXd'
-global.weem = 'BOTCAHX'
+global.mygit = 'https://github.com/Botwa021'
+global.webmy = 'https://botwa021.github.io'
+global.ghme = 'https://github.com/Botwa021'
+global.gcbot = "https://botwa021.github.io"
+global.botname = 'BALLBOT'
+global.omlen = '© Delaa'
+global.weem = 'BALLBOT'
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
